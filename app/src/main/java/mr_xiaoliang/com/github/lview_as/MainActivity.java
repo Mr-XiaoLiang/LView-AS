c1b001421f44259f761c684326d298e7db7f0a86
LiuJ <liujian594@hotmail.com>
2016-02-18 17:38:23 +0800

    2016-02-18 测试

--- app/src/main/java/mr_xiaoliang/com/github/lview_as/MainActivity.java
+++ app/src/main/java/mr_xiaoliang/com/github/lview_as/MainActivity.java
@@ -1,10 +1,11 @@
 package mr_xiaoliang.com.github.lview_as;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,10 +13,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
+        implements NavigationView.OnNavigationItemSelectedListener{
+    private Snackbar snackbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +29,16 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                snackbar = Snackbar.make(view, "GitHub：https://github.com/Mr-XiaoLiang/LView-AS", Snackbar.LENGTH_SHORT);
+                snackbar.setAction("复制", new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        ClipboardManager myClipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+                        String text = "https://github.com/Mr-XiaoLiang/LView-AS";
+                        ClipData myClip = ClipData.newPlainText("git",text);
+                        myClipboard.setPrimaryClip(myClip);
+                    }
+                });
             }
         });
 
@@ -41,6 +51,8 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
+
+
 
     @Override
     public void onBackPressed() {

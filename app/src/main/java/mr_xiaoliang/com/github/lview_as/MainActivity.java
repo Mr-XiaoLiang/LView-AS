ff9e5765f2a9f92520f35229d921a7d21423daf3
LiuJ <liujian594@hotmail.com>
2016-03-04 18:08:30 +0800

    2016-03-04

--- app/src/main/java/mr_xiaoliang/com/github/lview_as/MainActivity.java
+++ app/src/main/java/mr_xiaoliang/com/github/lview_as/MainActivity.java
@@ -9,19 +9,18 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
 import mr_xiaoliang.com.github.lview_as.dialog.CalendarDialog;
 import mr_xiaoliang.com.github.lview_as.dialog.LWheelDialog;
+import mr_xiaoliang.com.github.lview_as.util.ShortcutUtil;
 import mr_xiaoliang.com.github.lview_as.view.LCalendarView;
 import mr_xiaoliang.com.github.lview_as.view.LClockView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener,MainAdapter.OnItemClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, MainAdapter.OnItemClickListener {
 
     private ClipData myClip;
     private ClipboardManager myClipboard;
@@ -29,19 +28,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private FloatingActionButton fab;
     private String[] names = {
             "日历选择-日期没有限制&全向拖动", "日历选择-日期有限制&单向", "选项卡切换-三角形", "选项卡切换-线形", "时间选择-小时",
-            "时间选择-分钟","加载等待动画", "饼图", "进度图", "雷达图",
+            "时间选择-分钟", "加载等待动画", "饼图", "进度图", "雷达图",
             "圆形图片", "滑动按钮", "温度计", "进度条按钮", "页面下方小点",
-            "tab小点", "日期滚轮", "时间滚轮", "全套滚轮","tab条形",
-            "倒计时View","商品列表","支付宝咻一咻","系统自带的抽屉用法演示","现在较流行的抽屉样式",
-            "带涟漪的Layout","渐变的View","通讯录" };
+            "tab小点", "日期滚轮", "时间滚轮", "全套滚轮", "tab条形",
+            "倒计时View", "商品列表", "支付宝咻一咻", "系统自带的抽屉用法演示", "现在较流行的抽屉样式",
+            "带涟漪的Layout", "渐变的View", "通讯录", "添加桌面图标", "删除桌面图标（部分机型可用）"};
     private RecyclerView recyclerView;
     private MainAdapter adapter;
     private DialogUtil dialogUtil;
     private Calendar calendar;
     private Intent intent;
+    private ShortcutUtil shortcutUtil;
+    private int icon = 1;
 
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -54,8 +56,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         recyclerView.setAdapter(adapter = new MainAdapter(names, this, this));
         dialogUtil = new DialogUtil();
         calendar = Calendar.getInstance();
+        shortcutUtil = new ShortcutUtil(this);
     }
-
 
 
     @Override
@@ -159,6 +161,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 intent = new Intent(this, PhoneBook.class);
                 startActivity(intent);
                 break;
+            case 28:
+                shortcutUtil.addShortcut(getString(R.string.app_name), icon+"", R.mipmap.ic_launcher, MainActivity.class);
+                icon++;
+                t("已添加");
+                break;
+            case 29:
+                shortcutUtil.deleteShortcut(getString(R.string.app_name));
+                t("已删除");
+                break;
             default:
                 intent = new Intent(this, ViewTest.class);
                 intent.putExtra("type", position - 7);
@@ -166,7 +177,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
-    private void t(String s){
+
+    private void t(String s) {
         Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
     }
+
 }

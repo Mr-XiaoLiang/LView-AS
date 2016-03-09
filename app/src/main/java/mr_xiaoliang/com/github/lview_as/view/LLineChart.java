8da1521c6a0d6761d54c828f9d679bfa0a79877b
LiuJ <liujian594@hotmail.com>
2016-03-09 10:57:38 +0800

    2016-03-09

---
+++ app/src/main/java/mr_xiaoliang/com/github/lview_as/view/LLineChart.java
@@ -0,0 +1,29 @@
+package mr_xiaoliang.com.github.lview_as.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by LiuJ on 2016/3/9.
+ * 折线图
+ */
+public class LLineChart extends View {
+
+
+
+    public LLineChart(Context context) {
+        this(context, null);
+    }
+
+    public LLineChart(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LLineChart(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    public class LLineChartBean{
+
+    }
+}

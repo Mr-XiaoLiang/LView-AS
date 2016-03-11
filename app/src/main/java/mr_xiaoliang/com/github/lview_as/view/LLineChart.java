96bf47b7ab2240c518839dfdaf7f0a15034835a6
LiuJ <liujian594@hotmail.com>
2016-03-11 10:40:07 +0800

    2016-03-11

--- app/src/main/java/mr_xiaoliang/com/github/lview_as/view/LLineChart.java
+++ app/src/main/java/mr_xiaoliang/com/github/lview_as/view/LLineChart.java
@@ -1,16 +1,56 @@
 package mr_xiaoliang.com.github.lview_as.view;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
+
+import java.util.ArrayList;
 
 /**
  * Created by LiuJ on 2016/3/9.
  * 折线图
+ * |
+ * |         /\
+ * |    /\  /  \
+ * |   /  \/    \
+ * |  /          \
+ * |_/_____________
+ *
  */
 public class LLineChart extends View {
-
-
+    /**
+     * 最大值
+     */
+    private float maxNum;
+    /**
+     * 外壳颜色
+     */
+    private int shellColor = Color.WHITE;
+    /**
+     * 刻度颜色
+     */
+    private int scaleColor = Color.WHITE;
+    /**
+     * 数据
+     */
+    private ArrayList<LLineChartBean> beans;
+    /**
+     * 标签
+     */
+    private String[] lable;
+    /**
+     * 刻度数量
+     */
+    private int scaleSize;
+    /**
+     * 是否可以拉动
+     */
+    private boolean canSlide = true;
+    /**
+     * 横坐标偏移量，用来滑动
+     */
+    private int offset;
 
     public LLineChart(Context context) {
         this(context, null);
@@ -24,6 +64,7 @@ public class LLineChart extends View {
         super(context, attrs, defStyleAttr);
     }
     public class LLineChartBean{
-
+        float[] lable;
+        int color;
     }
 }
